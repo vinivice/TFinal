c599a7fee671d77b1086650a51ae3770332162dd
Vinícius Carlos Oliveira de Andrade <vinivice@gmail.com>
2020-01-24 13:18:35 -0300

    added kernel F1 SoA

---
+++ kernel_F1_SoA.cu
@@ -0,0 +1,240 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<algorithm>
+
+#include <curand.h>
+#include <curand_kernel.h>
+#include <thrust/sort.h>
+#include <thrust/device_ptr.h>
+#include <thrust/device_vector.h>
+
+
+//#define PSIZE 10
+//#define NGEN 500000
+#define MUT_PROB 0.05
+
+struct Individual 
+{
+    float fitness;
+    unsigned int chromossomes;
+};
+
+struct Population
+{
+	float *fitness;
+	unsigned int *chromossomes;
+};
+
+
+__host__ __device__ bool operator<(const Individual &i, const Individual &j)
+{
+    return (i.fitness > j.fitness);
+}
+
+__host__ __device__ float operator+(const Individual &i, const Individual &j)
+{
+    return (i.fitness + j.fitness);
+}
+
+__host__ __device__ Individual individualSum (Individual i, Individual j)
+{
+    Individual I;
+    I.fitness = i.fitness + j.fitness;
+    return I;
+}
+
+void printPop(Individual *population, int popSize, int print)
+{
+    if(print != 0)
+    {
+        for(int i = 0; i < popSize; i++)
+        {
+            printf("%f - ", population[i].fitness);
+        }
+    }
+}
+
+__global__ void createPopulation(Population *population, unsigned int seed, curandState_t *states)
+{
+    int id = blockIdx.x*blockDim.x + threadIdx.x;
+    curand_init(seed, id, 0, &states[id]);
+
+    population->fitness[id] = 0;
+    population->chromossomes[id] = curand(&states[id]);
+}
+
+__global__ void fitness(Population *population, float *totalFitness)
+{
+    int id = blockIdx.x;
+    unsigned int mask = 0x3FF;
+    __shared__ float a[3];
+
+    a[threadIdx.x] = (population->chromossomes[id] & (mask << (10 * threadIdx.x))) >> (10 * threadIdx.x);
+
+    a[threadIdx.x] = (a[threadIdx.x] - 512)/100.0;
+
+    __syncthreads();
+
+    if(threadIdx.x == 0)
+    {
+        float f = 1.0 / (1 + a[0]*a[0] + a[1]*a[1] + a[2]*a[2]);
+        population->fitness[id] = f;
+        atomicAdd(totalFitness, f);
+    }
+}
+
+__global__ void reproduce(Population *population, Population *nextPopulation, int PSIZE, float *totalFitness, curandState_t *states)
+{ 
+    int id = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int parents[2];
+    int temp = -1;
+    float localTotalFitness = *totalFitness;
+    //Selection
+    for(int j = 0; j < 2; j++)
+    {
+        float p = curand_uniform(&states[id]) * localTotalFitness;
+        float score = 0;
+
+        for(int k = 0; k < PSIZE; k++)
+        {
+            if(k == temp)
+            {
+                continue;
+            }
+            score += population->fitness[k];
+            if(p < score)
+            {
+		//HERE!!!!!!!!!!!!
+                parents[j] = population->chromossomes[k];
+                localTotalFitness -= population->fitness[k];
+                temp = k;
+                break;
+            }
+        }
+    }
+
+    //Crossover
+    unsigned char cutPoint = curand(&states[id]) % 31;
+    unsigned mask1 = 0xffffffff << cutPoint; 
+    unsigned mask2 = 0xffffffff >> (32 - cutPoint);
+    unsigned int child;
+    child = (parents[0] & mask1) + (parents[1] & mask2);
+
+    //Mutation
+    float mutation = curand_uniform(&states[id]);
+    if(mutation < MUT_PROB)
+    {
+        unsigned char mutPoint = curand(&states[id]) % 30;
+        child ^= 1 << mutPoint;
+    }
+
+
+
+    nextPopulation->chromossomes[id] = child;
+    nextPopulation->fitness[id] = 0;
+    if(id == 0)
+    {
+        nextPopulation->chromossomes[0] = population->chromossomes[0];
+    }
+
+
+}
+
+
+
+
+int main(int argc, char *argv[ ]) 
+{
+    int PSIZE, NGEN, NIT, PRINT;
+    double Ttotal = 0;
+    if(argc < 5)
+    {
+        printf("Uso %s <POP_SIZE> <N_GEN> <N_ITERACOES> <PRINT>\n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        PSIZE = atoi(argv[1]);
+        NGEN = atoi(argv[2]);
+        NIT = atoi(argv[3]);
+        PRINT = atoi(argv[4]);
+    }
+
+    for(int it = 0; it < NIT; it++)
+    {
+        clock_t start, end;
+        //Init variables
+
+        Population *population, *nextPopulation, *swap;
+
+        cudaMalloc((void**) &population, sizeof(Population));
+        cudaMalloc((void**) &nextPopulation, sizeof(Population));
+
+        cudaMalloc((void**) population->fitness, PSIZE * sizeof(float));
+        cudaMalloc((void**) population->chromossomes, PSIZE * sizeof(unsigned int));
+        cudaMalloc((void**) nextPopulation->fitness, PSIZE * sizeof(float));
+        cudaMalloc((void**) nextPopulation->chromossomes, PSIZE * sizeof(unsigned int));
+
+        curandState_t *states;
+        cudaMalloc((void**) &states, PSIZE * sizeof(curandState_t));
+
+
+        float *maxFitness;
+        maxFitness = (float *) malloc(NGEN * sizeof(float));
+
+
+        float *totalFitness;
+        cudaMalloc((void**) &totalFitness, sizeof(float));
+
+	start = clock();
+
+        //Create population
+        createPopulation<<<ceil(PSIZE/1024.0), min(PSIZE, 1024)>>>(population, time(NULL), states);
+
+        float const zero = 0.0f;
+        for(int i = 0; i < NGEN; i++)
+        {
+            cudaMemcpy(totalFitness, &zero, sizeof(float), cudaMemcpyHostToDevice);
+            //Calculate fitness
+            fitness<<<PSIZE, 3>>>(population, totalFitness);
+
+            //thrust::device_ptr<Population> dev_ptr_population(population);
+            //thrust::sort(dev_ptr_population, dev_ptr_population + PSIZE);
+            thrust::sort_by_key(population->fitness, population->fitness + PSIZE, population->chromossomes);
+            cudaMemcpy(&maxFitness[i], &(population->fitness[0]), sizeof(float), cudaMemcpyDeviceToHost);
+
+            reproduce<<<ceil(PSIZE/1024.0), min(PSIZE, 1024)>>>(population, nextPopulation, PSIZE, totalFitness, states);
+                
+            swap = population;
+            population = nextPopulation;
+            nextPopulation = swap;
+        }
+  
+        end = clock();
+        cudaFree(population);
+        cudaFree(nextPopulation);
+        cudaFree(states);
+
+        if(PRINT != 0)
+        {   
+            printf("Gen\tFitness\n");
+            for(int i = 0; i < NGEN; i++)
+            {
+                printf("%d\t%f\n", i+1, maxFitness[i]);
+            }
+        }
+
+        free(maxFitness);
+
+        printf("\nT total(us)\t\tT geração(us)\n");
+        double cpu_time_used = 1000000 * ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("%f\t\t%f\n\n", cpu_time_used, cpu_time_used/NGEN);
+        Ttotal += cpu_time_used;
+    }
+
+    printf("\nAvg T total(us)\t\tAvg T geração(us)\n");
+    printf("%f\t\t%f\n", Ttotal/NIT, Ttotal/(NIT*NGEN));
+    
+return 0;
+}
